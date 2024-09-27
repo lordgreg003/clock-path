71e9f0864fc714634daf4182bed28f40593017e9
Dannynsikak <nsikakdanny11@gmail.com>
2024-09-27 11:32:20 +0100

    implemented the header design and also modified the README.md

---
+++ src/app/ui/landingpage/MainSection.tsx
@@ -0,0 +1,7 @@
+export default function langingPage() {
+  return (
+    <div className="bg-[url('/images/Doodle Pattern Palette 2.svg')] bg-cover bg-center h-screen">
+      hello world
+    </div>
+  );
+}
