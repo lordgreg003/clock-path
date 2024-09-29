b6e9e460d6c0fd0be1de94ee79bc9e416a995de6
Dannynsikak <nsikakdanny11@gmail.com>
2024-09-29 18:37:14 +0100

    modified the responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ import Link from "next/link";
 export default function MyHeader() {
   return (
     <>
-      <header className="  p-[2em_0] border-b border-gray-300">
-        <div className="w-[90%] m-[0_auto] max-w-[1629px]">
-          <nav className="flex gap-2 lg:gap-0 justify-between items-center lg:*:w-[90%]">
+      <header className="p-[2em_0] border-b border-gray-300">
+        <nav className="w-[90%] m-[0_auto] max-w-[1629px]">
+          <div className="flex gap-2 lg:gap-0 justify-between items-center lg:*:w-[90%]">
             <div className="flex gap-2">
               <Image
                 src="/images/icons/clock.svg"
@@ -36,13 +36,13 @@ export default function MyHeader() {
             <div className="hidden lg:flex lg:*:flex-1 lg:gap-5 ">
               <Link
                 href="/contactUs"
-                className="border-2 border-[#6200EE] p-[.5em_1em] rounded-xl text-[#6200EE]"
+                className="border-2 border-[#6200EE] p-[.5em_1em] rounded-xl text-[#6200EE] text-center"
               >
                 Contact Us
               </Link>
               <Link
                 href="/login"
-                className="bg-[#6200EE] text-white p-[.5em_1.5em] rounded-xl"
+                className="bg-[#6200EE] text-white p-[.5em_1.5em] rounded-xl text-center"
               >
                 Login
               </Link>
@@ -56,12 +56,12 @@ export default function MyHeader() {
                 className="w-8 h-8 "
               />
             </button>
-          </nav>
-        </div>
+          </div>
+        </nav>
       </header>
       <main>
         <section className={`${styles.my_element} `}>
-          <div className="w-[85%] m-[0_auto] p-[3em_0] ">
+          <div className="w-[80%] m-[0_auto] pt-[5em] ">
             <div className="space-y-10 leading-tight">
               <div className="text-center space-y-5">
                 <h1
@@ -102,17 +102,16 @@ export default function MyHeader() {
                     Schedule a Demo
                   </button>
                 </div>
-
-                {/* Image Section */}
-                <div className="flex justify-center h-[400px]">
-                  <div className="self-end">
+                <div className="flex h-[350px]">
+                  {/* Image Section */}
+                  <div className=" self-end">
                     {/* Large screen image */}
                     <Image
                       src="/images/Group 39924.svg"
                       width={1282}
                       height={940}
                       alt="image of different clockpath screens"
-                      className="mx-auto hidden sm:block max-w-[100%]"
+                      className=" hidden sm:block"
                     />
                     {/* Small screen image */}
                     <Image
@@ -120,7 +119,7 @@ export default function MyHeader() {
                       width={362}
                       height={270}
                       alt="image of different clockpath screens"
-                      className="mx-auto block sm:hidden"
+                      className=" mx-auto block sm:hidden"
                     />
                   </div>
                 </div>
@@ -128,70 +127,76 @@ export default function MyHeader() {
             </div>
           </div>
         </section>
-        <section className="pt-[4em] ">
-          <div className="w-[90%] m-[0_auto]">
-            <div className="space-y-20 md:space-y-0 md:flex">
-              <div className="bg-blue-500">
-                <h1
-                  className={`${playfair_Display.className} font-bold text-[1.5rem] text-center`}
-                >
-                  Time Tracking Simplified
-                </h1>
-                <p
-                  className={`${open_sans.className} font-semibold text-[1rem] text-[#5C5C5C] text-center`}
-                >
-                  Track Time, Stay Efficient
-                </p>
-                <div
-                  className={`${open_sans.className} font-normal text-[.8rem] text-[#5C5C5C] flex gap-5 mt-[3em] md:mt-0`}
-                >
-                  <Image
-                    src="/images/Frame 1618868008.png"
-                    width={48}
-                    height={48}
-                    alt="a clock logo"
-                    className="self-start"
-                  />
-                  <p className="w-[50%] max-w-[300px]">
-                    Employees can clock in and out with ease using the ClockPath
-                    mobile app
+        <section className="p-[4em_0]">
+          <div className="w-[90%] m-[0_auto] max-w-[1750px]">
+            <div className="space-y-20 md:space-y-0 md:flex md:justify-between md:gap-10">
+              <div className="space-y-4 leading-tight">
+                <div className="">
+                  <h1
+                    className={`${playfair_Display.className} font-bold text-[2rem] text-center md:text-left relative`}
+                  >
+                    <span className="before:block before:absolute before:left-0 before:bottom-[0.15em] before:h-[0.40em] before:w-[65%] before:bg-[#FFCF88] before:z-[-10]">
+                      Time Tracking Simplified
+                    </span>
+                  </h1>
+                  <p
+                    className={`${open_sans.className} font-semibold text-[1.5rem] text-[#5C5C5C] text-center md:text-left`}
+                  >
+                    Track Time, Stay Efficient
                   </p>
-                </div>{" "}
-                <div
-                  className={`${open_sans.className} font-normal text-[.8rem] text-[#5C5C5C] flex gap-5`}
-                >
-                  {" "}
-                  <Image
-                    src="/images/Frame 1618868008 (1).png"
-                    width={48}
-                    height={48}
-                    alt="a clock logo"
-                    className="self-start"
-                  />
-                  <p className="w-[50%] max-w-[300px]">
-                    GPS-based manual entries ensure accuracy for remote teams
-                  </p>
-                </div>{" "}
-                <div
-                  className={`${open_sans.className} font-normal text-[.8rem] text-[#5C5C5C] flex gap-5`}
-                >
-                  <div>
-                    {" "}
+                </div>
+                <div className="space-y-5">
+                  <div
+                    className={`${open_sans.className} font-normal text-[1rem] text-[#5C5C5C] flex gap-5 mt-[3em] md:mt-0`}
+                  >
                     <Image
-                      src="/images/Frame 1618868008 (2).png"
+                      src="/images/Frame 1618868008.png"
                       width={48}
                       height={48}
                       alt="a clock logo"
                       className="self-start"
                     />
+                    <p className="w-[50%]  max-w-[700px] ">
+                      Employees can clock in and out with ease using the
+                      ClockPath mobile app
+                    </p>
+                  </div>{" "}
+                  <div
+                    className={`${open_sans.className} font-normal text-[1rem] text-[#5C5C5C] flex gap-5`}
+                  >
+                    {" "}
+                    <Image
+                      src="/images/Frame 1618868008 (1).png"
+                      width={48}
+                      height={48}
+                      alt="a clock logo"
+                      className="self-start"
+                    />
+                    <p className="w-[50%] max-w-[700px]">
+                      GPS-based manual entries ensure accuracy for remote teams
+                    </p>
+                  </div>{" "}
+                  <div
+                    className={`${open_sans.className} font-normal text-[1rem] text-[#5C5C5C] flex gap-5`}
+                  >
+                    <div>
+                      {" "}
+                      <Image
+                        src="/images/Frame 1618868008 (2).png"
+                        width={48}
+                        height={48}
+                        alt="a clock logo"
+                        className="self-start"
+                      />
+                    </div>
+                    <p className="w-[50%]">
+                      View detailed clock-in and clock-out histories
+                    </p>
                   </div>
-                  <p className="w-[50%]">
-                    View detailed clock-in and clock-out histories
-                  </p>
                 </div>
               </div>
               <div>
-                <div className="bg-[#EFE6FD] rounded-xl h-[500px] flex justify-center">
+                <div className="bg-[#EFE6FD] rounded-xl h-[400px] flex justify-center">
                   {" "}
                   <Image
                     src="/images/Dashboard.png"
