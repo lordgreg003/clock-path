94b1f5e953f61e33d597d8356395341c70f11d3c
chidera nwokoye <chideranwokoye555@gmail.com>
2024-10-15 13:37:04 +0100

    payment

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Payment from "../ui/payment/Payment";
+
+const page = () => {
+  return (
+    <div>
+      <Payment />
+    </div>
+  );
+};
+
+export default page;
