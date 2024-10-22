fc1fdfba44d94c75d5a3609ed3520e2eb0576020
chidera nwokoye <chideranwokoye555@gmail.com>
2024-10-22 17:23:32 +0100

    modified[upload/setup]

--- src/app/ui/component/uploadEmployee/UploadEmployees.tsx
+++ src/app/ui/component/uploadEmployee/UploadEmployees.tsx
@@ -1,6 +1,9 @@
 "use client";
 import React, { useState } from "react";
-import { FiUpload } from "react-icons/fi"; // Importing an upload icon from react-icons
+import { FiUpload } from "react-icons/fi";
+import { open_sans, playfair_Display } from "@/app/ui/fonts/fonts";
+import Link from "next/link";
+import { GrFormNextLink } from "react-icons/gr";
 
 const UploadEmployees: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -20,12 +23,26 @@ const UploadEmployees: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-50 px-4">
+    <div className="flex flex-col justify-center items-center h-screen bg-primary-primary50 px-4">
+      <div className="w-full flex justify-end my-4">
+        <Link href="/payment" className="flex items-center mr-5">
+          <div className="text-[#6200EE] font-semibold hover:underline cursor-pointer">
+            Skip
+          </div>
+          <div>
+            <GrFormNextLink className="text-2xl text-[#6200EE]" />
+          </div>
+        </Link>
+      </div>
       <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
-        <h2 className="text-2xl font-semibold text-center mb-4">
+        <h2
+          className={`${playfair_Display.className} text-[#333333] text-[28px] font-bold text-center mb-4`}
+        >
           Upload Employees
         </h2>
-        <p className="text-gray-600 text-center mb-6">
+        <p
+          className={`${open_sans.className} font-normal text-[#5C5C5C] text-center mb-6`}
+        >
           Enter your company details in a few simple steps to get started with
           ClockPath.
         </p>
@@ -35,12 +52,14 @@ const UploadEmployees: React.FC = () => {
           <div className="border-dashed border-2 border-gray-300 p-6 rounded-lg text-center mb-4 hover:border-indigo-500 transition duration-200">
             <FiUpload className="mx-auto text-gray-400 mb-4 text-4xl" />{" "}
             {/* Upload Icon */}
-            <p className="text-gray-600 mb-2">
+            <p
+              className={`${open_sans.className} text-[#333333] font-normal mb-2`}
+            >
               Drag and drop to upload Employee List
             </p>
-            <p className="text-sm text-gray-400">
+            <p className={`${open_sans} text-sm text-[#98A2B3]`}>
               EXCEL or CSV (
-              <a href="#" className="text-indigo-500">
+              <a href="#" className="font-semibold text-[14px] text-[#6200EE]">
                 View Sample
               </a>
               )
@@ -52,7 +71,7 @@ const UploadEmployees: React.FC = () => {
             </div>
             <label
               htmlFor="fileInput"
-              className="bg-white border border-black text-[#6200EE] py-2 px-4 rounded-lg cursor-pointer hover:bg-indigo-50"
+              className=" border border-[#6200EE] text-[#6200EE] py-2 px-4 rounded-lg cursor-pointer "
             >
               Browse Files
             </label>
@@ -73,7 +92,7 @@ const UploadEmployees: React.FC = () => {
 
           <button
             type="submit"
-            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="w-full bg-[#EFE6FD] text-[#CEB0FA] py-2 px-4 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             Next
           </button>
